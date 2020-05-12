0b2833bf1f025696098796a5042467f6b2abc656
Kevin Holder <kevholderx@gmail.com>
2020-05-12 18:29:28 -0400

    fix: dont erase decimal prop in addreses.json when deploying/setting up new tokens

--- src/other-tokens.js
+++ src/other-tokens.js
@@ -2,8 +2,12 @@ module.exports = {
   mainnet: [
     { address: "TMwFHYXLJaRUPeW6421aqXL4ZEzPRFGkGT", code: "USDJ" },
     { address: "TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9", code: "JST" },
-    { address: "TBhxyECmAg3uCqqmEHQvGJbrgj9cn1yMZ1", code: "TEWKEN" },
-    { address: "TNp9BX9NMjsmWPZugw13VisP1oVJT2pMDv", code: "DCD" },
+    {
+      address: "TBhxyECmAg3uCqqmEHQvGJbrgj9cn1yMZ1",
+      code: "TEWKEN",
+      decimals: 6,
+    },
+    { address: "TNp9BX9NMjsmWPZugw13VisP1oVJT2pMDv", code: "DCD", decimals: 6 },
     { address: "TPpLkxGeKragRC7qpiQjjtNmf6shXWi8i9", code: "FRAG" },
   ],
 };
