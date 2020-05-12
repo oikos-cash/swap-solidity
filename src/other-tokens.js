dd9a8382aad03da7f2415daf96fe78042c30886d
Kevin Holder <kevholderx@gmail.com>
2020-05-12 17:46:41 -0400

    Add frag exchange

--- src/other-tokens.js
+++ src/other-tokens.js
@@ -4,5 +4,6 @@ module.exports = {
     { address: "TCFLL5dx5ZJdKnWuesXxi1VPwjLVmWZZy9", code: "JST" },
     { address: "TBhxyECmAg3uCqqmEHQvGJbrgj9cn1yMZ1", code: "TEWKEN" },
     { address: "TNp9BX9NMjsmWPZugw13VisP1oVJT2pMDv", code: "DCD" },
+    { address: "TPpLkxGeKragRC7qpiQjjtNmf6shXWi8i9", code: "FRAG" },
   ],
 };
